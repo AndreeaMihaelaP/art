d1bb1e7290694179f44a17f3d6b3f3c3b4d7591e
AndreeaMihaelaP <andreeapatrascu20@yahoo.com>
2021-11-30 17:09:20 +0000

    Fixed nav issue

--- src/screens/navigation/Routes.js
+++ src/screens/navigation/Routes.js
@@ -1,4 +1,4 @@
-import { Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Gallery from "../gallery/Gallery";
 import Exhibitions from "../exhibitions/Exhibitions";
@@ -9,14 +9,16 @@ import Category from "screens/gallery/Category";
 
 const Routes = () => {
   return (
-    <Switch>
-      <Route path="/gallery" component={Gallery} />
-      <Route path="/exhibitions" component={Exhibitions} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-      <Route path="/home" component={Home} />
-      <Route path="/category" component={Category} />
-    </Switch>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/gallery" component={Gallery} />
+        <Route exact path="/exhibitions" component={Exhibitions} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/contact" component={Contact} />
+        <Route exact path="/home" component={Home} />
+        <Route exact path="/category" component={Category} />
+      </Switch>
+    </Router>
   );
 };
 
